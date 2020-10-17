96b8dda2d83e85d8f25faf23a3aab78e9050650a
jonathan joly <joly534@gmail.com>
2020-10-17 08:54:32 +0200

    update

--- texte_sur_lcd/texte_sur_lcd.ino
+++ texte_sur_lcd/texte_sur_lcd.ino
@@ -7,7 +7,7 @@ LiquidCrystal lcd(11,12,5,4,3,2); // liaison 4 bits de données
 void setup()
 {
    lcd.begin(16,2); // utilisation d'un écran 16 colonnes et 2 lignes
-   lcd.print("Tu boudes ?"); // petit test pour vérifier que tout marche
+   lcd.print("hello world !"); // petit test pour vérifier que tout marche
 }
 
 void loop() {
